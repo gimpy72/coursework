dbee7ce7eb346fb6403ced22ed7d9697ae526152
Brandy Clark <gimpy72@gmail.com>
2011-04-25 13:11:07 -0500

    removed nil statements and added formulas

--- ltp/ch01/numbers01.rb
+++ ltp/ch01/numbers01.rb
@@ -1,15 +1,15 @@
 # Replace "nil" with math to calculate the number of hours in a year
-hours_in_year = nil
+hours_in_year = 24*365
 puts "There are #{hours_in_year} hours in a year"
 
 # Replace "nil" with math to calculate the number of minutes in a decade
-minutes_in_decade = nil
+minutes_in_decade = 60*24*(365*10+2)
 puts "There are #{minutes_in_decade} minutes in a decade"
 
 # Replace "nil"
-seconds_old = nil
+seconds_old = 60*60*24*(365*38+9)
 puts "I am #{seconds_old} seconds old"
 
 # Replace "nil"
-chocolates_in_lifetime = nil
+chocolates_in_lifetime = 2*(365*80)
 puts "I hope to eat #{chocolates_in_lifetime} chocolates in my lifetime"
