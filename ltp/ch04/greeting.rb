f647a5e4fe8cc6e05e825c80a120dfbf7b3f02af
Brandy Clark <gimpy72@gmail.com>
2011-04-25 19:22:56 -0500

    completed ch04 exercises

--- ltp/ch04/greeting.rb
+++ ltp/ch04/greeting.rb
@@ -1,2 +1,10 @@
 # Write a program which asks for a person's first name, then middle, then last.
 # Finally, it should greet the person using their full name.
+puts 'What is your first name?'
+f_name = gets.chomp
+puts 'What is your middle name?'
+m_name = gets.chomp
+puts 'What is your last name?'
+l_name = gets.chomp
+full_name = f_name + ' ' + m_name + ' ' + l_name
+puts 'Hello, ' + full_name + '!'
