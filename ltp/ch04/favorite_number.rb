7c621c3aa63d4fb5b92006611205a76476b10c4f
Brandy Clark <gimpy72@gmail.com>
2011-04-27 19:39:04 -0500

    completed ch04 exercises

--- ltp/ch04/favorite_number.rb
+++ ltp/ch04/favorite_number.rb
@@ -1,7 +1,6 @@
 # Write a program which asks for a person's favorite number. Have your program
 # add one to the number, then suggest the result as a bigger and better
 # favorite number. (Do be tactful about it, though.)
-# brandy's program
 puts 'What is your favorite number?'
 f_num = gets.chomp.to_i
 new_num = f_num + 1
--- ltp/ch04/greeting.rb
+++ ltp/ch04/greeting.rb
@@ -1,6 +1,5 @@
 # Write a program which asks for a person's first name, then middle, then last.
 # Finally, it should greet the person using their full name.
-# brandy's program
 puts 'What is your first name?'
 f_name = gets.chomp
 puts 'What is your middle name?'
